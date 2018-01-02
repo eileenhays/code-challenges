8254ed46f28b506d9e7749f20561609dd93ea425
Eileen Hays-Schwantes <eileen.hays@gmail.com>
2018-01-02 12:12:52 -0800

    linked list and recursion problem

--- ctci-data-structures.py
+++ ctci-data-structures.py
@@ -12,14 +12,14 @@ def array_left_rotation(a, n, k):
     return a
 
 def array_left_rotation2(a, n, k):
-	new_a = []
-	i = 0
+    new_a = []
+    i = 0
 
-	for i in range(n): 
-		new_idx = (i + k) % n
-		new_a.append(a[new_idx])
+    for i in range(n): 
+        new_idx = (i + k) % n
+        new_a.append(a[new_idx])
 
-	return new_a
+    return new_a
         
 
 # n, k = map(int, raw_input().strip().split(' '))
@@ -55,28 +55,26 @@ def number_needed(a, b):
 print number_needed('cde', 'abc') == 4 
 print number_needed('abbc', 'bda') == 3 
 
-## Hash Tables: Randsom Note
-def ransom_note(m, n, magazine, ransom):
+## Hash Tables: Ransom Note
+
+from collections import defaultdict
+
+def ransom_note2(m, n, magazine, ransom):
     if n > m:
         return False
     
-    mag_dict = {}
+    mag_dict = defaultdict(int)
     
     for word in magazine:
-        if word not in mag_dict:
-            mag_dict[word] = 1
-        else: 
-            mag_dict[word] += 1
+        mag_dict[word] += 1
     
     for word in ransom:
-        if word not in mag_dict:
+        if mag_dict[word] == 0:
             return False
-        elif mag_dict[word] == 0:
-            return False
-        else:
-            mag_dict[word] -= 1
+        mag_dict[word] -= 1
         
-    return True            
+    return True 
+
 
 # m, n = map(int, raw_input().strip().split(' '))
 # magazine = raw_input().strip().split(' ')
@@ -88,5 +86,49 @@ def ransom_note(m, n, magazine, ransom):
 #     print "No"
 
 # Tests
-print ransom_note(6, 4, 'give me one grand today night', 'give one grand today') == True
-print ransom_note(6, 5, 'two times three is not four', 'two times two is four') == False
+print ransom_note2(6, 4, 'give me one grand today night', 'give one grand today') == True
+print ransom_note2(6, 5, 'two times three is not four', 'two times two is four') == False
+
+## Linked Lists: Detect a Cycle
+
+"""
+Detect a cycle in a linked list. Note that the head pointer may be 'None' if the list is empty.
+
+A Node is defined as: 
+"""
+ 
+class Node(object):
+    def __init__(self, data = None, next_node = None):
+        self.data = data
+        self.next = next_node
+
+def has_cycle(head, visited=[]):
+
+    node = head
+    
+    if node == None:
+        return False
+    elif node.next == None:
+        return False
+    elif node in visited:
+        return True
+    
+    visited.append(node)
+    return has_cycle(node.next, visited)
+
+# Tests
+no_cycle_ll = Node(4, Node(5, Node(6, None)))
+node_1 = Node(1, None)
+node_2 = Node(2, None) 
+node_3 = Node(3, None)
+node_1.next = node_2
+node_2.next = node_3
+node_3.next = node_2  
+cycle_ll = node_1
+
+print has_cycle(no_cycle_ll) == False
+print has_cycle(cycle_ll) == True
+
+
+
+
